39c5f84fcd99a74294e0e2872de0bc0ea85323f1
FeDeJMendez <fedemendezg@gmail.com>
2021-07-12 22:26:00 -0300

    Add Details & Spinners

---
+++ src/components/MoviesGrid.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { get } from "../utils/httpClient";
+import { MovieCard } from "./MovieCard";
+import styles from "./MoviesGrid.module.css";
+import { Spinner } from "./Spinner";
+
+export function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    setisLoading(true);
+    get("/discover/movie").then((data) => {
+      setMovies(data.results);
+      setisLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <ul className={styles.moviesGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+}
